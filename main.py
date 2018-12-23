0732417253fde5ea1874e860cb1535ef7c0f26f1
鸡毛 <shihao.gu@perfma.com>
2018-12-23 19:03:37 +0800

    softmax函数曲线

--- main.py
+++ main.py
@@ -4,8 +4,18 @@
 import numpy as np;
 import matplotlib.pyplot as plt;
 
-x = np.arange(-6, 6, 0.1);
-y = x * x;
+def sigmoid (x):
+    return 1 / (1 + np.exp(-x));
+
+def softmax (x):
+    c = np.max(x);
+    expA = np.exp(x - c);
+    sumExpA = np.sum(expA);
+    y = expA / sumExpA;
+    return y;
+
+x = np.arange(0, 100, 1);
+y = softmax(x);
 
 plt.plot(x, y, label = "sin");
 plt.xlabel("x");
